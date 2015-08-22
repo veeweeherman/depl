8d148339fa47da690eade7215b566d396a425cfc
veeweeherman <vy.hoang.cu@gmail.com>
2015-08-21 21:48:56 -0700

    added index html page

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-app.get('/', function(req, res){
-  res.send('testing');
-});
-// app.get('/', function(req, res) {
-//     res.sendFile(path.join(__dirname + '/index.html'));
+// app.get('/', function(req, res){
+//   res.send('testing');
 // });
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 app.listen(3000);
